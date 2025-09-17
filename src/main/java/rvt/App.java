0bfe6213be22d1119ff2c2d93993b2e7d368265e
24DP1OLudv <A240815OL@rvt.lv>
2025-09-17 09:20:46 +0300

    Average of three numbers (1 pusgrupa)

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,7 +3,7 @@ package rvt;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Ieraksti pirmo skaitli:");
@@ -11,10 +11,13 @@ public class App {
 
         System.out.println("Ievadiet otro skaitli:");
         int otr_skait = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Ievadiet treso skaitli:");
+        int tre_skait = Integer.valueOf(scanner.nextLine());
         scanner.close();
 
-        int summa = pirm_skait + otr_skait;
+        double videjais = Double.valueOf((pirm_skait + otr_skait + tre_skait) / 3.0);
 
-        System.out.println(pirm_skait + " + " + otr_skait + " = " + summa);
+        System.out.println("Videjais skaitlis ir " + videjais);
     }
 }
