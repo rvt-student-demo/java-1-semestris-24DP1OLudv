3625878dbc4bbdcaa4fc7b2925b51190be3bc438
24DP1OLudv <A240815OL@rvt.lv>
2025-09-17 09:07:47 +0300

    Addition formula

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,20 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ieraksti pirmo skaitli:");
+        int pirm_skait = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Ievadiet otro skaitli:");
+        int otr_skait = Integer.valueOf(scanner.nextLine());
+        scanner.close();
+
+        int summa = pirm_skait + otr_skait;
+
+        System.out.println(pirm_skait + " + " + otr_skait + " = " + summa);
     }
 }
