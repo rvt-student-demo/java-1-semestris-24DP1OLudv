0c6a6b06e284e6e0ad130f098f6698467ec15f80
24DP1OLudv <A240815OL@rvt.lv>
2025-09-18 12:48:07 +0300

    Odd or even

---
+++ src/main/java/rvt/OddOrEven.java
@@ -0,0 +1,20 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Iedod numuru:");
+        int numurs = Integer.valueOf(scanner.nextLine());
+
+        if (numurs % 2 == 0) {
+            System.out.println("Number " + numurs + " is even");
+        }
+        else {
+            System.out.println("Number" + numurs + " is odd");
+        }
+
+    }
+}
