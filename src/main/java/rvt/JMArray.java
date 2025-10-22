3e5dee4268951fdcb51eb799f1eafe7cc19d73b7
24DP1OLudv <A240815OL@rvt.lv>
2025-10-22 09:48:24 +0300

    Commit+

--- src/main/java/rvt/JMArray.java
+++ src/main/java/rvt/JMArray.java
@@ -1,12 +1,14 @@
 package rvt;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class JMArray {
     public static void main(String[] args) {
-        OnlyTheseNumbers();
+        RemoveLast(new ArrayList<String>());
     }
+
 
     public static void OnlyTheseNumbers() {
         Scanner scanner = new Scanner(System.in);
@@ -14,19 +16,81 @@ public class JMArray {
         int numberInp = 0;
 
         System.out.println("Ievadiet skaitlus:");
-        while (true){
+        while (true) {
             numberInp = Integer.valueOf(scanner.nextLine());
             if (numberInp != -1) {
                 list.add(numberInp);
-            }
-            else {
+            } else {
                 break;
             }
         }
-        for (int i = 0; i < list.size(); i++) {
+        System.out.print("No kurienes? ");
+        int no = Integer.valueOf(scanner.nextLine());
+        System.out.print("Lidz kurienei? ");
+        int lidz = Integer.valueOf(scanner.nextLine());
+
+        for (int i = no; i <= lidz; i++) {
             System.out.println(list.get(i));
         }
-        scanner.close();
     }
-}
 
+    public static void ListSize() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        System.out.println("Ievadiet vardus: ");
+        while (true) {
+            String nameInp = String.valueOf(scanner.nextLine());
+            if (nameInp == "") {
+                break;
+            } else {
+                list.add(nameInp);
+            }
+        }
+        int total = list.size();
+        System.out.print("In total: " + total);
+    }
+
+    public static void OnTheList() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        System.out.println("Ievadiet vardus: ");
+        while (true) {
+            String nameInp = String.valueOf(scanner.nextLine());
+            if (nameInp == "") {
+                break;
+            } else {
+                list.add(nameInp);
+            }
+        }
+        System.out.println(list);
+        System.out.print("Kadu vardu just meklejat? ");
+        String search = String.valueOf(scanner.nextLine());
+
+        if (list.contains(search) == true) {
+            System.out.println(search + " tika atrasts!");
+        } else {
+            System.out.println(search + " netika atrasts!");
+        }
+
+    }
+
+    public static void RemoveLast(ArrayList<String> strings) {
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+        RemoveMethod(strings);
+        RemoveMethod(strings);
+        System.out.println(strings);
+    }
+
+    public static void RemoveMethod(List<?> list) {
+        if (!list.isEmpty()) {
+            list.remove(list.size() - 1);
+        }
+    }
+
+}
